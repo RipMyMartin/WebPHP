5ba3f23e3651db991e43683fa9de9c285f2475d8
RipMyMartin <martinsild.mr@gmail.com>
2024-11-04 10:43:24 +0200

    tekst kui massiv functsioonid

--- proov.php
+++ proov.php
@@ -110,4 +110,5 @@ echo '<br>';
 $tekst = 'All thinking men are atheists';
 print_r(str_word_count($tekst, 2));
 //Array ( [0] => All [4] => thinking [13] => men [17] => are [21] => atheists )
+
 ?>
